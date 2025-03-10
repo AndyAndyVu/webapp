e49f005e1d2a841b5a387e7c7b695afc475ca202
AndyAndyVu <andyandyvu@gmail.com>
2025-03-10 11:34:24 +0100

    tilføjet et array som har objekter inde i sig
    Prøver at få den til at loope igennem hvor og filter efter tid, artist, location og pris

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+const events = [
+    { 
+        time: "12:00", 
+        artist: "Late Night Jam", 
+        location: "Abzalon Event Factory", 
+        price: "50,-"
+    },
+    {
+        time: "15:00", 
+        artist: "Big Creek Slim & Johnny Burgin", 
+        location: "Kulturcenter Fjordbyen", 
+        price: "free" 
+    },
+    { 
+        time: "18:00",
+        artist: "Sahra Da Silva Band", 
+        location: "Salling Rooftop", 
+        price: "free" 
+    },
+    { 
+        time: "20:00",  
+        artist: "Brønderslev Big Band", 
+        location: "Jazzscenen på Gammeltorv", 
+        price: "free" 
+    },
+    { 
+        time: "22:00", 
+        artist: "HP Lange & Jarno Varsted", 
+        location: "Bluesscenen på Toldbod Plads", 
+        price: "free" 
+    },
+];
